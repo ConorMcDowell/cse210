0b6f08887443c0158193fe0fc37daf8bf021390d
ConorMcDowell <mcd22009@byui.edu>
2025-01-14 17:18:47 -0700

    prep3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int mag_num = randomGenerator.Next(1, 100);
+
+        int guess_num;
+        int i = 1;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guess_num = int.Parse(guess);
+
+            if (guess_num > mag_num)
+            {
+                Console.WriteLine("Lower");
+                i++;
+            }
+            else if (guess_num < mag_num)
+            {
+                Console.WriteLine("Higher");
+                i++;
+            }
+            else if (guess_num == mag_num)
+            {
+                Console.WriteLine("That's Correct!");
+                Console.WriteLine($"It took you {i} guesses.");
+                Console.Write("Would you like to play again?");
+                string ans = Console.ReadLine();
+                if (ans == "yes")
+                {
+                    Main(args);
+                }
+                else
+                {
+                    Console.WriteLine("Goodbye!");
+                }
+            }
+            
+        }while (mag_num != guess_num);
     }
+
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers. To stop, type 0.");
+
+        int num;
+        int sum = 0;
+        int max = 0;
+        int total = 0;
+        do
+        {
+            Console.Write("Enter a number: ");
+            string number = Console.ReadLine();
+            num = int.Parse(number);
+            numbers.Add(num);
+        } while(num != 0);
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            sum += numbers[i];
+            total = i;
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+        }
+        float avg = ((float) sum / numbers.Count);
+
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+        Console.WriteLine($"The max of the numbers is: {max}");
+        Console.WriteLine($"The average of the numbers is: {avg}");
     }
 }
