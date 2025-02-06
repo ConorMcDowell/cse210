1c66d5678a14839f757a5d90e9d6d3e1d82f0c82
ConorMcDowell <mcd22009@byui.edu>
2025-02-05 21:06:43 -0700

    fraction

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,47 @@
+class Fraction
+{
+    private int _top;
+    private int _bottom;
+    public Fraction ()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction (int num)
+    {
+        _top = num;
+        _bottom = 1;
+    }
+    public Fraction (int num, int den)
+    {
+        _top = num;
+        _bottom = den;
+    }
+
+    public int GetTopNum ()
+    {
+        return _top;
+    }
+    public int GetBottomNum ()
+    {
+        return _bottom;
+    }
+    public void SetTopNum (int numerator)
+    {
+        _top = numerator;
+    }
+    public void SetBottomNum (int denominator)
+    {
+        _bottom = denominator;
+    }
+
+    public string GetFractionString ()
+    {
+
+        return $"{GetTopNum()}/{GetBottomNum()}";
+    }
+    public double GetDecimalValue ()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -3,7 +3,22 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
+    {   
+        int i = 1;
+        while (i < 2)
+        {
+            Console.WriteLine("Enter the numerator: ");
+            string numerator = Console.ReadLine();
+            int num = int.Parse(numerator);
+
+            Console.WriteLine("Enter the denominator: ");
+            string denominator = Console.ReadLine();
+            int den = int.Parse(denominator);
+
+            Fraction fraction1 = new Fraction(num, den);
+            
+            Console.WriteLine($"{fraction1.GetFractionString()}");
+            Console.WriteLine($"{fraction1.GetDecimalValue()}");
+        }
     }
 }
